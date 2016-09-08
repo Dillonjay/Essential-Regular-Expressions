873e576a302e340c7c8149c1bddfffda9ead6e9b
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-08 15:46:01 -0500

    U.S. postal code validation complete

--- USA/US-Postal-Code.js
+++ USA/US-Postal-Code.js
@@ -4,12 +4,26 @@
 // Ex: 
 // 78704 or 78704-6666 or 78704 6666.
 function isZipValid(zipCode) {
-	var zip = /^\d{5}(?:[-\s]\d{4})?$/
-	zip.test(zipCode) ? console.log('This is a valid zipcode') : console.log('This is not a valid zipcode')
+	var zip = /^\d{5}(?:[-\s]\d{4})?$/;
+	zip.test(zipCode) ? console.log('This is a valid postal code') : console.log('This is not a valid postal code');
 };
 
 // This function will only allow a 5 digit postal code.
+// EX:
+// 78789
 function isZipValid(zipCode) {
-	var zip = /^\d{5}/
+	var zip = /^\d{5}$/;
+	zip.test(zipCode) ? console.log('This is a valid postal code') : console.log('This is not a valid postal code');
 };
+
 // This function will only allow a 9 digit postal code.
+// Allows either a space or a dash to separate digits.
+// Ex:
+// 58485-324 or 23433 3435
+function isZipValid(zipCode) {
+	var zip = /^\d{5}(?:[-\s])?\d{4}$/;
+	zip.test(zipCode) ? console.log('This is a valid postal code') : console.log('This is not a valid postal code');
+};
+
+
+
